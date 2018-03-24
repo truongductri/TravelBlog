f2dd2f01b743e30a15b65f2c6b19116965103651
truongductri <clevolove.unik37@gmail.com>
2018-03-24 16:35:30 +0700

    Add project files.

---
+++ SimplBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimplBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
