50691d1950246a4782a78ed0ea8562e0faed6c33
truongductri <clevolove.unik37@gmail.com>
2018-04-08 18:47:01 +0700

    create new database by migration

---
+++ SimplBlog/Migrations/20180408113909_CreateNewDB.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SimplBlog.Migrations
+{
+    public partial class CreateNewDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    ImagePath = table.Column<string>(nullable: true),
+                    Meta = table.Column<string>(nullable: true),
+                    Modified = table.Column<DateTime>(nullable: true),
+                    PostedOn = table.Column<DateTime>(nullable: false),
+                    Published = table.Column<bool>(nullable: false),
+                    ShortDescription = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    TagId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Count = table.Column<int>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Sort = table.Column<int>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TagId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
--- SimplBlog/Models/BlogDbContext.cs
+++ SimplBlog/Models/BlogDbContext.cs
@@ -12,29 +12,48 @@ namespace SimplBlog.Models
         public BloggingContext(DbContextOptions<BloggingContext> options)
             : base(options)
         { }
-        public DbSet<CategoryBl> CategoryBls { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
-        
+        public DbSet<Tag> Tags { get; set; }
+
     }
 
-    public class CategoryBl
+    public class Category
     {
-        [Key]
-        public int CateBlId { get; set; }
+        
+        public int CategoryId { get; set; }
         public string Url { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        
+    }
+    public class Tag
+    {
+       
+        public int TagId { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? Count { get; set; }
+        public int? Sort { get; set; }
 
-        public List<Post> Posts { get; set; }
+
     }
 
     public class Post
     {
         public int PostId { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description  { get; set; }
+        public string Meta { get; set; }
+        public string Url  { get; set; }
+        public bool Published { get; set; }
+        public DateTime PostedOn { get; set; }
+        public DateTime? Modified { get; set; }
+        public string ImagePath { get; set; }
 
-        public int CateBlId { get; set; }
-        
+       
     }
 
    
